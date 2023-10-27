d68b5a4eacb4967a759f7680b896998d4ba57ca0
Muhammed A. Mahmoud <54907641+muteufit@users.noreply.github.com>
2023-10-27 11:31:31 +0200

    Update data_plotter.py

--- data_plotter.py
+++ data_plotter.py
@@ -1,13 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-print('Please use Excel file format with Columns that has first rows as X, Y for the data.')
-"""
-Obey that format in Excel
- X     |Y
- 0.5541|99.2
- 0.5632|991
-"""
 excel_file = input('Enter excel file name with extension: ')
 
 df = pd.read_excel(excel_file)
